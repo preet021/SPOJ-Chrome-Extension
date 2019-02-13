1a0bf1e2d3d56af135ed82a7eaf9830ffa3fb682
Preet Thakkar <preet.thakkar@students.iiit.ac.in>
2019-02-14 01:55:18 +0530

    README initialized

--- popup.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-	chrome.tabs.query(
-		{currentWindow: true, active: true},
-		function (tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, Object.keys(tabs[0]))
-		}
-	)
-}, false);
